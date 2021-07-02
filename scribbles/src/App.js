522105946151628e3e25e6f8f9e008021160e790
Senik <senik@Seniks-MacBook-Pro.local>
2021-07-02 15:20:31 +0200

    scribbles  part2c :tada:

--- scribbles/src/App.js
+++ scribbles/src/App.js
@@ -1,10 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
 const App = (props) => {
-  const [ notes, setNotes ] = useState(props.notes)
+  const [ notes, setNotes ] = useState([])
   const [ newNote, setNewNote ] = useState("a new note...")
   const [ showAll, setShowAll ] = useState(true)
+  const hook = () => {
+    axios.get('http://localhost:3001/notes').then(response => {
+      console.log('fulfilled')
+      setNotes(response.data)
+    })
+  }
+  useEffect(hook,[])
+  console.log('render', notes.length, 'notes')
   const handleChange = (event) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
